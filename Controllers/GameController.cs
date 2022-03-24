5e61f9e037c283f43e8cfe2adb29db3225ab0b68
WilliamWHN <william.hausmann@cpnv.ch>
2022-03-24 08:55:16 +0100

    Add game controller

---
+++ Controllers/GameController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Wordle.Models;
+
+namespace Wordle.Controllers
+{
+    [Authorize]
+    public class GameController : Controller
+    {
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Word word = new Word();
+            return View("index", word);
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
