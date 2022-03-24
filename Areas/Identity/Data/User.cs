d7a380b9e2b441fe7e42441470805dd63cfed41b
WilliamWHN <william.hausmann@cpnv.ch>
2022-03-24 08:55:48 +0100

    Add game's page with some js

--- Areas/Identity/Data/User.cs
+++ Areas/Identity/Data/User.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Wordle.Models;
 
 namespace Wordle.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the User class
-public class AuthUser : IdentityUser
+public class User : IdentityUser
 {
 }
 
---
+++ wwwroot/js/game.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+
+    let attempt = 1
+    let box = 0
+    let wordToGuess = $("#word").val()
+    let word = ""
+
+    $(document).on('keydown', function (event) {
+        var regex = new RegExp("^[a-zA-Z]$");
+        var key = event.key
+        let letterBox = $(".attempt-" + attempt).children(".case-" + box).children()
+        if (!regex.test(key)) {
+            if (key == "Backspace" && box > 0) {
+                box--
+                letterBox = $(".attempt-" + attempt).children(".case-" + box).children()
+                letterBox.text("")
+                word = word.slice(0,-1)
+            }
+            else if (key == "Enter") {       
+                validateWord(word)
+            }
+            else {
+                event.preventDefault()
+                return false;
+            }
+        }
+        else {
+            if (box < wordToGuess.length) {
+                letterBox.text(key.toUpperCase())
+                word += key.toLowerCase()
+                box++
+            }
+        }
+
+        
+    });
+
+    function validateWord(word) {
+        if (word === wordToGuess) {
+            alert("AH OUI OUI OUI, c'est gagné !")
+        }
+    }
+});
