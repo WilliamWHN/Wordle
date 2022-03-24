39163ef3346ce2ea83eed8935b23b9745bdb5085
WilliamWHN <william.hausmann@cpnv.ch>
2022-03-24 08:53:50 +0100

    Get word with the api

---
+++ Models/Word.cs
@@ -0,0 +1,29 @@
+﻿namespace Wordle.Models
+{
+    public class Word
+    {
+        public string word;
+        public int size;
+        private readonly HttpClient client;
+
+        public Word()
+        {
+            client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            GetWordAsync().Wait();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<String> GetWordAsync()
+        {
+            word = await client.GetStringAsync("https://random-word-api.herokuapp.com/word");
+            word = word.Split('"')[1];
+            size = word.Length;
+
+            return word;
+        }
+    }
+}
